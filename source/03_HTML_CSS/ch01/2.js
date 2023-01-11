244ba8c2c4ece5ba07e695270e3331224de99de0
95dongju <95.dongju@gmail.com>
2023-01-11 09:37:39 +0900

    230111

--- source/03_HTML_CSS/ch01/2.js
+++ source/03_HTML_CSS/ch01/2.js
@@ -1,5 +1,5 @@
-// 2.js : ������ �κ�
-name = prompt("What's your name?", "hong"); // ��Ҹ� Ŭ���ϸ� 'null'' ����
-if (name != 'null' && name != '') { // �� ��Ʈ�� Ȥ�� null �� ���� ��µ��� �ʵ���
-    document.write(name + "~ Welcome<br>")
+// 2.js : 동적인 부분
+name = prompt("이름은?", "홍길동"); // 취소를 클릭하면 'null'' 리턴
+if (name != 'null' && name != '') { // 빈 스트링 혹은 null 이 들어가면 출력되지 않도록
+    document.write(name + "~ 반갑습니다<br>")
 }
