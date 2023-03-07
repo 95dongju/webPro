0223e9c57cd55f080b19610623e4f7b0425de83f
95dongju <95.dongju@gmail.com>
2023-03-07 17:42:05 +0900

    final

---
+++ source/06_jsp/ch19_mvc/src/com/lec/service/MemberlistService.java
@@ -0,0 +1,20 @@
+package com.lec.service;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.lec.dao.MemberDao;
+import com.lec.dto.MemberDto;
+
+public class MemberlistService implements Service {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+		// list 로직
+		MemberDao mDao = MemberDao.getInstance();
+		ArrayList<MemberDto> memberList = mDao.listMember();
+		request.setAttribute("memberList", memberList);
+	}
+}
